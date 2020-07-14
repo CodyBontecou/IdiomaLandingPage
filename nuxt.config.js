63c86befde85adc47d4cbd6d0799d19486243857
CodyBontecou <bontecouc@gmail.com>
2020-07-13 21:16:34 -0500

    added minor meta changes

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,17 +14,17 @@ export default {
    ** See https://nuxtjs.org/api/configuration-head
    */
   head: {
-    title: process.env.npm_package_name || '',
+    title: 'Spanish Learning',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       {
         hid: 'description',
         name: 'description',
-        content: process.env.npm_package_description || '',
+        content: 'Spanish Learning',
       },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/icon.png' }],
   },
   /*
    ** Global CSS
