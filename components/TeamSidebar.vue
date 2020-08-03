14e307931adaabec616d5e6f37b7fab1014320e3
CodyBontecou <bontecouc@gmail.com>
2020-08-02 22:01:59 -0500

    Updated team page to have a similar style to syllabus

---
+++ components/TeamSidebar.vue
@@ -0,0 +1,66 @@
+<template>
+  <v-navigation-drawer v-show="$vuetify.breakpoint.mdAndUp" app clipped>
+    <v-list dense nav class="pt-10">
+      <v-list-item class="pb-5">
+        <nuxt-link to="/team" class="list-title-link">
+          <span class="text-h6 font-weight-bold">Team</span>
+        </nuxt-link>
+      </v-list-item>
+
+      <v-list-item
+        v-for="item in team"
+        :key="item.id"
+        :to="item.to"
+        dense
+        nuxt
+        color="primary"
+      >
+        <v-list-item-content>
+          <v-list-item-title class="text-capitalize subtitle-1">
+            {{ item.name }}
+          </v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+  name: 'TeamSidebar',
+  data: () => {
+    return {
+      team: [
+        {
+          name: 'Daniela Gonzalez Barron',
+          description: '',
+          image: '/daniela.jpg',
+          role: 'Founder',
+          to: '/team/daniela',
+        },
+        {
+          name: 'Maribi Garcia',
+          description: '',
+          image: '/maribi.jpg',
+          role: 'Founder',
+          to: '/team/maribi',
+        },
+        {
+          name: 'Cody Bontecou',
+          description: '',
+          image: '/cody.jpeg',
+          role: 'Software & Marketing',
+          to: '/team/cody',
+        },
+      ],
+    }
+  },
+}
+</script>
+
+<style scoped>
+.list-title-link {
+  text-decoration: none;
+  color: inherit;
+}
+</style>
--- pages/team/index.vue
+++ pages/team/index.vue
@@ -1,51 +1,41 @@
 <template>
   <v-main>
-    <v-container class="fill-height" fluid>
-      <v-row align="center" justify="top">
-        <v-col class="text-center" cols="12" xs="12">
-          <h1 class="text-uppercase font-weight-black text-h2 text-lg-h1">
-            Who we are
-          </h1>
-          <h2 class="text-h5 text-uppercase font-weight-medium py-4">
-            A language learning team that's different from the rest
-          </h2>
-          <v-card class="mx-auto" max-width="800" flat>
-            <v-card-text class="text-h6 font-weight-light">
-              We are a couple of entrepreneurs, intelligent, and studious women,
-              who seek to teach our language in a more colloquial and friendly
-              way, following the correct grammatical structure and providing a
-              broad and vast vocabulary for a full understanding of the
-              language.
-            </v-card-text>
-          </v-card>
-        </v-col>
-      </v-row>
-      <v-row align="center" justify="center">
-        <v-col
-          v-for="member in team"
-          :key="member.id"
-          xs="12"
-          md=""
-          cols="12"
-          class="text-center pa-0 ma-0"
-        >
-          <v-card class="mx-auto" max-width="255" flat>
-            <v-row align="center" justify="center" class="pa-0 ma-0">
-              <v-avatar size="150">
-                <v-img :src="member.image"></v-img>
-              </v-avatar>
-            </v-row>
-            <v-row align="center" justify="center">
-              <v-card-text class="font-weight-thin">
-                <h3 class="text-h5 font-weight-bold">
-                  {{ member.name }}
-                </h3>
-                <h3 class="text-h6 font-weight-bold primary--text">
-                  {{ member.role }}
-                </h3>
-              </v-card-text>
-            </v-row>
-          </v-card>
+    <v-container fluid>
+      <v-row align="start" justify="center">
+        <v-col xs="12" cols="12">
+          <div class="pt-6 px-md-16 mr-md-16">
+            <div class="px-16">
+              <DividedTitle :title="title" :subtitle="subtitle"></DividedTitle>
+              <v-row align="center" justify="center" class="pt-16">
+                <v-col
+                  v-for="member in team"
+                  :key="member.id"
+                  xs="12"
+                  md=""
+                  cols="12"
+                  class="text-center pa-0 ma-0"
+                >
+                  <v-card class="mx-auto" max-width="255" flat>
+                    <v-row align="center" justify="center" class="pa-0 ma-0">
+                      <v-avatar size="150">
+                        <v-img :src="member.image"></v-img>
+                      </v-avatar>
+                    </v-row>
+                    <v-row align="center" justify="center">
+                      <v-card-text class="font-weight-thin">
+                        <h3 class="text-h5 font-weight-bold">
+                          {{ member.name }}
+                        </h3>
+                        <h3 class="text-h6 font-weight-bold primary--text">
+                          {{ member.role }}
+                        </h3>
+                      </v-card-text>
+                    </v-row>
+                  </v-card>
+                </v-col>
+              </v-row>
+            </div>
+          </div>
         </v-col>
       </v-row>
     </v-container>
@@ -53,9 +43,22 @@
 </template>
 
 <script>
+import DividedTitle from '@/components/DividedTitle'
+
 export default {
+  components: {
+    DividedTitle,
+  },
   data: () => {
     return {
+      title: 'Who we are',
+      'title-2': "A language learning team that's different from the rest",
+      subtitle:
+        'We are a couple of entrepreneurs, intelligent, and studious people,\n' +
+        '              who seek to teach our language in a more colloquial and friendly\n' +
+        '              way, following the correct grammatical structure and providing a\n' +
+        '              broad and vast vocabulary for a full understanding of the\n' +
+        '              language.',
       team: [
         {
           name: 'Daniela Gonzalez Barron',
