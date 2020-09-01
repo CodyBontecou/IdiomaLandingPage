bd702f628ac3e08af257a4510b73a110395eeea6
Cody Bontecou <bontecouc@gmail.com>
2020-09-01 11:24:53 -0500

    modal button needed dark prop for white text

--- pages/index.vue
+++ pages/index.vue
@@ -71,7 +71,7 @@
               ></iframe>
               <v-toolbar bottom flat>
                 <v-spacer></v-spacer>
-                <v-btn color="primaryTwo" :to="localePath('signup')">
+                <v-btn color="primaryTwo" dark :to="localePath('signup')">
                   {{ $t('getStarted') }}
                 </v-btn>
               </v-toolbar>
